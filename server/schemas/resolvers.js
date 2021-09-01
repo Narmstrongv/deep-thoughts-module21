dedfe4ed9f3522fa197b9f6ff8899837d423f428
Newton <narmstrongv@gmail.com>
2021-08-31 18:52:25 -0700

    mod stuff

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,17 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    me: async (parent, args) => {
+      const userData = await User.findOne({})
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
+  
+      return userData;
+    },
     users: async () => {
       return User.find()
         .select('-__v -password')
@@ -20,6 +30,31 @@ const resolvers = {
     },
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
+    }
+  },
+  
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+    
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+    
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const correctPw = await user.isCorrectPassword(password);
+    
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const token = signToken(user);
+      return { token, user };
     }
   }
 };
